6bbee911cedd26a8a9c45ceea6ed7e48ca747a45
cheolyeing <34466581+cheolyeing@users.noreply.github.com>
2020-05-18 22:57:44 +0900

    #2822 점수계산

---
+++ N2822.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class N10707 {
+	static int A, B, C, D, P; 
+	public static void main(String[] args) throws IOException {
+		setting();
+		solution();
+	}
+	
+	static void setting () throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		A = Integer.parseInt(br.readLine());
+		A = Integer.parseInt(br.readLine());
+		A = Integer.parseInt(br.readLine());
+		A = Integer.parseInt(br.readLine());
+		A = Integer.parseInt(br.readLine());
+	}
+	
+	static void solution() {
+		
+	}
+}
