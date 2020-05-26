717cc9f5fa73205c9b2bda0eb5db76b06a7233f2
cheolyeing <34466581+cheolyeing@users.noreply.github.com>
2020-05-27 02:51:05 +0900

    #10282 해킹

--- N10282.java
+++ N10282.java
@@ -1,19 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-public class Standard {
-	static int n, d, c, time[], ans;
-	static boolean[] visited;
+public class N10282 {
+	static final int INF = Integer.MAX_VALUE;
+	static int n, d, c, time[];
 	static ArrayList<ArrayList<Pair>> com;
 	static class Pair implements Comparable<Pair> {
-		int a, b, s;
-		public Pair(int a, int b, int s) {
-			this.a = a; this.b = b; this.s = s;
+		int idx, cost;
+		public Pair(int a, int s) {
+			this.idx = a; this.cost = s;
 		}
 		@Override
 		public int compareTo(Pair p) {
 			// TODO Auto-generated method stub
-			return this.s - p.s;
+			return this.cost - p.cost;
 		}
 	}
 	public static void main(String[] args) throws IOException {
@@ -30,18 +30,17 @@ public class Standard {
 			n = Integer.parseInt(st.nextToken());
 			d = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
-			ans = 0;
 			time = new int[n+1];
-			Arrays.fill(time, Integer.MAX_VALUE);
-			visited = new boolean[n+1];
+			Arrays.fill(time, INF);
 			com = new ArrayList<ArrayList<Pair>>();
+			
 			for(int i=0; i<=n; i++) com.add(new ArrayList<Pair>());
 			for(int i=0; i<d; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int s = Integer.parseInt(st.nextToken());
-				com.get(b).add(new Pair(a, b, s));
+				com.get(b).add(new Pair(a, s));
 			}
 			dijkstra();
 		}
@@ -53,27 +52,26 @@ public class Standard {
 	
 	static void dijkstra() {
 		PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
-		visited[c] = true;
 		time[c] = 0;
-		
-		int t = 0;
-		for(Pair p : com.get(c)) pq.add(p);
+		pq.add(new Pair(c, 0));
 		
 		while(!pq.isEmpty()) {
-			Pair tmp = pq.poll();
-			time[tmp.a] = Math.min(time[tmp.a], time[tmp.b]+tmp.s);
-			if(visited[tmp.a]) continue;
-			else {
-				visited[tmp.a] = true;
-				for(Pair p : com.get(tmp.a)) pq.add(p);
+			Pair from = pq.poll();
+			
+			if(from.cost > time[from.idx]) continue;
+			for(Pair to : com.get(from.idx)) {
+				if(time[to.idx] > time[from.idx] + to.cost) {
+					time[to.idx] = time[from.idx] + to.cost;
+					pq.add(new Pair(to.idx, time[to.idx]));
+				}
 			}
 		}
 		
 		int cnt=0, max=0;
 		for(int i=1; i<=n; i++) {
-			if(visited[i]) {
-				max = Math.max(max, time[i]);
+			if(time[i]!=INF) {
 				cnt++;
+				max = Math.max(max, time[i]);
 			}
 		}
 		System.out.println(cnt+" "+max);
