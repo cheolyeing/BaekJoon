edd5a454f2314283b56ce1e979280bd420c07fb8
cheolyeing <34466581+cheolyeing@users.noreply.github.com>
2020-06-18 18:40:15 +0900

    #1197 최소 스패닝 트리

--- N1197.java
+++ N1197.java
@@ -3,40 +3,83 @@ import java.util.*;
 
 public class N1197 {
 
-    static int V, E;
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
-    static class Node {
-        public int V;
-        public int W;
-        public Node(int V, int W) {
-            this.V = V; this.W = W;
-        }
-    }
-
-    static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         init();
         solution();
+    }
+
+    static int v, e, root[], answer = 0;
+    static Node[] edge;
+
+    static class Node implements Comparable<Node> {
+        public int e1, e2, w;
+
+        public Node(int e1, int e2, int w) {
+            this.e1 = e1;
+            this.e2 = e2;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.w > node.w ? 1 : -1;
+        }
     }
 
     static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        root = new int[v + 1];
+        edge = new Node[e];
 
-        for(int i=0; i<=V; i++) graph.add(new ArrayList<Node>());
-        for(int i=0; i<E; i++) {
+        for (int i = 1; i <= v; i++) root[i] = i;
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
+            int e1 = Integer.parseInt(st.nextToken());
+            int e2 = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph.get(v1).add(new Node(v2, w));
-            graph.get(v2).add(new Node(v1, w));
+
+            edge[i] = new Node(e1, e2, w);
         }
     }
 
     static void solution() {
+        Arrays.sort(edge);
+        int visited = 0;
 
+        for (Node node : edge) {
+            int e1 = node.e1;
+            int e2 = node.e2;
+            int w = node.w;
+
+            if (visited == v - 1) break;
+            if (makeCycle(e1, e2)) continue;
+            union(e1, e2);
+
+            answer += w;
+            visited++;
+        }
+        System.out.println(answer);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            if (x < y) root[y] = x;
+            else root[x] = y;
+        }
+    }
+
+    static int find(int x) {
+        if (x == root[x]) return x;
+        else return root[x] = find(root[x]);
+    }
+
+    static boolean makeCycle(int x, int y) {
+        return find(x) == find(y);
     }
 }
